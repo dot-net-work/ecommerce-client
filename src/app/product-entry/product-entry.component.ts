bd48ca47ec8bd676f4ade401382aab7733bab5e0
shirajul.mamun <shirajul.mamun@gmail.com>
2019-10-12 19:24:07 +0600

    template driven forms in angular

--- src/app/product-entry/product-entry.component.ts
+++ src/app/product-entry/product-entry.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../Models/product.model';
 import { ProductService } from '../product.service';
 import { Category } from '../Models/cateogry.model';
 import { CategoryService } from '../category.service';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'product-entry',
@@ -13,6 +14,7 @@ export class ProductEntryComponent implements OnInit {
 
   constructor(private productService:ProductService, private categoryService: CategoryService) { 
     this.onSaved = new EventEmitter<Product>();
+    this.product.categoryId = 3;
   }
 
   ngOnInit() {
@@ -27,14 +29,17 @@ export class ProductEntryComponent implements OnInit {
 
   isSaved: boolean;
 
-  public save(){
+  public save(form: NgForm){
     //productmanager -> add(product)
+    Object.assign(this.product,form.value);
+
     this.productService.add(this.product).subscribe(p=>{
       this.isSaved = true;
       this.product.id = p.id;
       if(this.isSaved && this.onSaved!=null){
         this.onSaved.emit(this.product);
         this.product = new Product();
+        form = new NgForm(null,null);
       }
     });
    
